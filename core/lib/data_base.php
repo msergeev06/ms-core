552f9efffa1073fd678ce0f4ad378b6eddcad2f7
Mikhail Sergeev <msergeev06@gmail.com>
2017-06-08 11:02:52 +0300

    Добавлены функции отладки. Функция, возвращающая shell команду для дампа DB

--- core/lib/data_base.php
+++ core/lib/data_base.php
@@ -40,6 +40,16 @@ class DataBase {
 	 */
 	protected $db_conn;
 
+	protected $db_queries=0;
+
+	protected $db_query_start=0;
+
+	protected $db_query_stop=0;
+
+	protected $db_last_query_time=0;
+
+	protected $db_all_query_time=0;
+
 	/**
 	 * Конструктор. Осуществляет подключение к базе данных и передает начальные параметры подключения
 	 *
@@ -69,7 +79,9 @@ class DataBase {
 	public function query (Query $obQuery)
 	{
 		$sql = $obQuery->getQueryBuildParts();
+		$this->setQueryStart();
 		$db_res = mysql_query($sql, $this->db_conn);
+		$this->setQueryStop();
 
 		$res = new DBResult($db_res,$obQuery);
 		$res->setAffectedRows(mysql_affected_rows($this->db_conn));
@@ -85,5 +97,121 @@ class DataBase {
 
 		return $res;
 	}
+
+	/**
+	 * Возвращает время выполнения последнего SQL запроса
+	 *
+	 * @return float
+	 */
+	public function getLastQueryTime ()
+	{
+		return floatval($this->db_last_query_time);
+	}
+
+	/**
+	 * Возвращает общее время всех SQL запросов
+	 *
+	 * @return float
+	 */
+	public function getAllQueryTime ()
+	{
+		return floatval($this->db_all_query_time);
+	}
+
+	/**
+	 * Возвращает общее количество выполненных SQL запросов
+	 *
+	 * @return int
+	 */
+	public function getCountQuery ()
+	{
+		return intval($this->db_queries);
+	}
+
+	public function getDumpCommand ($path,$postfix=null,$package=null,$arTables=array(),$useGz=true,$pastDate=true,$noData=false,$dbName=null,$dbUser=null,$dbPass=null)
+	{
+		$comm = 'mysqldump ';
+		//$comm .= '-Q -c -e ';
+		if ($noData === true)
+		{
+			$comm .= '--no-data ';
+		}
+		if (is_null($dbName))
+		{
+			$dbName = $this->base;
+		}
+		if (is_null($dbUser))
+		{
+			$dbUser = $this->user;
+		}
+		if (is_null($dbPass))
+		{
+			$dbPass = $this->pass;
+		}
+		$comm .= "-u".$dbUser." -p".$dbPass." ".$dbName." ";
+		if (!empty($arTables))
+		{
+			foreach ($arTables as $tableName)
+			{
+				$comm .= $tableName.' ';
+			}
+		}
+		if ($useGz === true)
+		{
+			$comm .= '| gzip ';
+		}
+		$comm .= '> ';
+/*		if ($pastDate === true)
+		{
+			$comm .= '`date +';
+		}*/
+		$comm .= $path.'dump_'.$dbName;
+		if (!is_null($package))
+		{
+			$comm .= '_'.$package;
+		}
+		if (!empty($arTables))
+		{
+			$comm .= '_tables';
+		}
+		if (!is_null($postfix) && $postfix !== false)
+		{
+			$comm .= '_'.$postfix;
+		}
+		if ($pastDate === true)
+		{
+			//$comm .= '.%Y%m%d.%H%M%S';
+			$comm .= date('.Ymd.His');
+		}
+		$comm .= '.sql';
+		if ($useGz === true)
+		{
+			$comm .= '.gz';
+		}
+
+		return $comm;
+	}
+
+	/**
+	 * Устанавливает начальное время SQL запроса и увеличивает общее количество запросов на 1
+	 */
+	private function setQueryStart ()
+	{
+		$this->db_query_start = microtime (true);
+		$this->db_queries++;
+	}
+
+	/**
+	 * Устанавливает конечное время SQL запроса, вычисляет время выполнения последнего запроса и время выполнения всех запросов
+	 */
+	private function setQueryStop ()
+	{
+		$this->db_query_stop = microtime(true);
+
+		$this->db_last_query_time = $this->db_query_stop - $this->db_query_start;
+
+		$this->db_all_query_time += $this->db_last_query_time;
+	}
+
 
 }

c43c4f7898d5183e65737a82e0e00f6d90dafe2b
Mikhail Sergeev <msergeev06@gmail.com>
2017-06-08 12:01:02 +0300

    Добавлены языковые файлы

---
+++ core/loc/ru/tools.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Локализация ядра
+ *
+ * @package MSergeev\Core
+ * @subpackage Loc
+ * @author Mikhail Sergeev <msergeev06@gmail.com>
+ * @copyright 2016 Mikhail Sergeev
+ */
+
+return array(
+	'day1'   => 'день',
+	'day2'    => 'дня',
+	'day3'    => 'дней'
+);
---
+++ core/loc/ru/webix.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Локализация ядра
+ *
+ * @package MSergeev\Core
+ * @subpackage Loc
+ * @author Mikhail Sergeev <msergeev06@gmail.com>
+ * @copyright 2016 Mikhail Sergeev
+ */
+
+return array(
+	'no_data_to_view' => 'Нет данных для отображения...'
+);
